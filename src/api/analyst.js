dfc087451ead2beb2a99b52f29fc9e8168a70993
treeoverflow <97726189+treeoverflow@users.noreply.github.com>
2022-11-17 21:06:26 +0200

    DD-4: Add eslint to project (#5)
    add eslint to the project, implement its code style

--- src/api/analyst.js
+++ src/api/analyst.js
@@ -1,28 +1,27 @@
 require('dotenv').config();
 const needle = require('needle');
+
 const token = process.env.TWITTER_API_KEY;
 const endpointURL = 'https://api.twitter.com/2/tweets/counts/recent';
 
 const getMentions = async (keyword, startTime, endTime) => {
-    
-    const params = {
-        query: keyword
-    }
-    if (startTime) params.start_time = startTime;
-    if (endTime) params.end_time = endTime;
+  const params = {
+    query: keyword,
+  };
+  if (startTime) params.start_time = startTime;
+  if (endTime) params.end_time = endTime;
 
-    const res = await needle('get', endpointURL, params, {
-        headers: {
-            "User-Agent": "v2TweetLookupJS",
-            "authorization": `Bearer ${token}`
-        }
-    });
+  const res = await needle('get', endpointURL, params, {
+    headers: {
+      'User-Agent': 'v2TweetLookupJS',
+      authorization: `Bearer ${token}`,
+    },
+  });
 
-    if (res.body) {
-        return res.body;
-    } else {
-        throw new Error('Unsuccessful request');
-    }
-}
+  if (res.body) {
+    return res.body;
+  }
+  throw new Error('Unsuccessful request');
+};
 
-module.exports = { getMentions }
+module.exports = { getMentions };
--- src/util/date-util.js
+++ src/util/date-util.js
@@ -1,17 +1,16 @@
 class DateUtil {
+  static isFutureTime(sourceMs, receivedMs) {
+    return sourceMs - receivedMs < 0;
+  }
 
-    static isFutureTime(sourceMs, receivedMs) {
-        return sourceMs - receivedMs < 0;
-    }
+  static isPastTime(sourceMs, receivedMs) {
+    return sourceMs - receivedMs > 0;
+  }
 
-    static isPastTime(sourceMs, receivedMs) {
-        return sourceMs - receivedMs > 0;
-    }
-
-    static isTimeOlderByDays(sourceMs, receivedMs, daysAmount) {
-        const oneDayMs = 86_400_000;
-        return (sourceMs - receivedMs) > (oneDayMs * daysAmount);
-    }
+  static isTimeOlderByDays(sourceMs, receivedMs, daysAmount) {
+    const oneDayMs = 86_400_000;
+    return (sourceMs - receivedMs) > (oneDayMs * daysAmount);
+  }
 }
 
 module.exports = DateUtil;
