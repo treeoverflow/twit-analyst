6d1e7ec1a20abd7401c9632e831bf5bfa73e24f2
treeoverflow <treeoverflow@gmail.com>
2022-11-21 00:26:18 +0200

    integrate express-validator

--- app.js
+++ app.js
@@ -1,73 +1,76 @@
 const express = require('express');
+const { query, validationResult } = require('express-validator');
 const { getMentions } = require('./src/api/analyst');
 const DateUtil = require('./src/util/date-util');
 
 const app = express();
 const port = 3000;
 const keywordRegexp = /^[^\W_]{1,512}$/;
+const maxObservableDays = 7;
 
-function isValidKeyword(keyword) {
-  return keyword && keywordRegexp.test(keyword);
+function isValidStartTime(timeISO) {
+  const currentTimeMs = Date.now();
+  const startTimeMs = Date.parse(timeISO);
+  return DateUtil.isPastTime(currentTimeMs, startTimeMs)
+    && !DateUtil.isTimeOlderByDays(currentTimeMs, startTimeMs, maxObservableDays);
 }
 
-app.get('/mentions', async (req, res) => {
-  const { filterBy: keyword, startTime, endTime } = req.query;
+function isValidEndTime(timeISO) {
   const currentTimeMs = Date.now();
-  const maxObservableDays = 7;
-  let startTimeISO;
-  let endTimeISO;
-  let startTimeMs;
-  let endTimeMs;
+  const endTimeMs = Date.parse(timeISO);
+  return !DateUtil.isFutureTime(currentTimeMs, endTimeMs)
+    && !DateUtil.isTimeOlderByDays(currentTimeMs, endTimeMs, maxObservableDays);
+}
 
-  if (!isValidKeyword(keyword)) {
-    res.send('filterBy should exist');
-    return;
-  }
+function isEndTimeComingAfterStartTime() {
+  return (endTime, { req }) => {
+    const { startTime } = req.query;
+    return DateUtil.isFutureTime(Date.parse(startTime), Date.parse(endTime));
+  };
+}
 
-  if (startTime) {
-    startTimeMs = Date.parse(startTime);
-    if (Number.isNaN(startTimeMs)) {
-      res.send('startTime should be ISO 8601 format');
+app.get(
+  '/mentions',
+  query('filterBy')
+    .exists()
+    .withMessage('filterBy should exist')
+    .matches(keywordRegexp)
+    .withMessage('filterBy has invalid value'),
+
+  query('startTime')
+    .optional()
+    .isISO8601()
+    .withMessage('startTime should be ISO 8601 format')
+    .custom((startTime) => isValidStartTime(startTime))
+    .withMessage('startTime should be past time and be no more than 7 days ago'),
+
+  query('endTime')
+    .optional()
+    .isISO8601()
+    .withMessage('endTime should be ISO 8601 format')
+    .custom((endTime) => isValidEndTime(endTime))
+    .withMessage('endTime should be present or past time and be no more than 7 days ago')
+    .if(query('startTime').exists())
+    .custom(isEndTimeComingAfterStartTime())
+    .withMessage('endTime should be after startTime'),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
       return;
     }
 
-    if (!DateUtil.isPastTime(currentTimeMs, startTimeMs)) {
-      res.send('startTime should be past time');
-      return;
-    }
+    const { filterBy: keyword, startTime, endTime } = req.query;
+    let startTimeISO;
+    let endTimeISO;
 
-    if (DateUtil.isTimeOlderByDays(currentTimeMs, startTimeMs, maxObservableDays)) {
-      res.send('startTime should be no more than 7 days ago');
-      return;
-    }
-    startTimeISO = new Date(startTime).toISOString();
-  }
+    if (startTime) startTimeISO = new Date(startTime).toISOString();
+    if (endTime) endTimeISO = new Date(endTime).toISOString();
 
-  if (endTime) {
-    endTimeMs = Date.parse(endTime);
-    if (Number.isNaN(endTimeMs)) {
-      res.send('endTime should be ISO 8601 format');
-      return;
-    }
-
-    if (DateUtil.isFutureTime(currentTimeMs, endTimeMs)) {
-      res.send('endTime should be present or past time');
-      return;
-    }
-
-    if (DateUtil.isTimeOlderByDays(currentTimeMs, endTimeMs, maxObservableDays)) {
-      res.send('endTime should be no more than 7 days ago');
-      return;
-    }
-
-    if (startTime && !DateUtil.isFutureTime(startTimeMs, endTimeMs)) {
-      res.send('endTime should be after startTime');
-    }
-    endTimeISO = new Date(endTime).toISOString();
-  }
-
-  const mentionData = await getMentions(keyword, startTimeISO, endTimeISO);
-  res.send(mentionData);
-});
+    const mentionData = await getMentions(keyword, startTimeISO, endTimeISO);
+    res.send(mentionData);
+  },
+);
 
 app.listen(port);
