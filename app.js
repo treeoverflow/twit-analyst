02004e271ea0975530459422798f1611f49caf87
treeoverflow <97726189+treeoverflow@users.noreply.github.com>
2022-11-25 16:11:40 +0200

    DD-6: integrate express-validator (#7)
    integrate express-validator

--- app.js
+++ app.js
@@ -1,73 +1,85 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const { getMentions } = require('./src/api/analyst');
 const DateUtil = require('./src/util/date-util');
 
 const app = express();
 const port = 3000;
-const keywordRegexp = /^[^\W_]{1,512}$/;
+const keywordPattern = /^[^\W_]{1,512}$/;
+const maxObservableDays = 7;
 
-function isValidKeyword(keyword) {
-  return keyword && keywordRegexp.test(keyword);
+function isValidStartTime(currentTimeMs, timeISO) {
+  const startTimeMs = Date.parse(timeISO);
+  return DateUtil.isPastTime(currentTimeMs, startTimeMs)
+    && !DateUtil.isTimeOlderByDays(currentTimeMs, startTimeMs, maxObservableDays);
 }
 
-app.get('/mentions', async (req, res) => {
-  const { filterBy: keyword, startTime, endTime } = req.query;
-  const currentTimeMs = Date.now();
-  const maxObservableDays = 7;
-  let startTimeISO;
-  let endTimeISO;
-  let startTimeMs;
-  let endTimeMs;
+function isValidEndTime(currentTimeMs, timeISO) {
+  const endTimeMs = Date.parse(timeISO);
+  return !DateUtil.isFutureTime(currentTimeMs, endTimeMs)
+    && !DateUtil.isTimeOlderByDays(currentTimeMs, endTimeMs, maxObservableDays);
+}
 
-  if (!isValidKeyword(keyword)) {
-    res.send('filterBy should exist');
-    return;
-  }
+function isEndTimeComingAfterStartTime() {
+  return (endTime, { req }) => {
+    const { startTime } = req.query;
+    return DateUtil.isFutureTime(Date.parse(startTime), Date.parse(endTime));
+  };
+}
 
-  if (startTime) {
-    startTimeMs = Date.parse(startTime);
-    if (Number.isNaN(startTimeMs)) {
-      res.send('startTime should be ISO 8601 format');
+app.get(
+  '/mentions',
+
+  async (req, res) => {
+    let currentTimeMs;
+
+    await check('filterBy')
+      .exists()
+      .withMessage('filterBy should exist')
+      .bail()
+      .matches(keywordPattern)
+      .withMessage('filterBy has invalid value')
+      .run(req);
+
+    if (req.query.startTime || req.query.endTime) currentTimeMs = Date.now();
+
+    await check('startTime')
+      .optional()
+      .isISO8601()
+      .withMessage('startTime should be ISO 8601 format')
+      .bail()
+      .custom((startTime) => isValidStartTime(currentTimeMs, startTime))
+      .withMessage('startTime should be past time and be no more than 7 days ago')
+      .run(req);
+
+    await check('endTime')
+      .optional()
+      .isISO8601()
+      .withMessage('endTime should be ISO 8601 format')
+      .bail()
+      .custom((endTime) => isValidEndTime(currentTimeMs, endTime))
+      .withMessage('endTime should be present or past time and be no more than 7 days ago')
+      .if(check('startTime').exists())
+      .custom(isEndTimeComingAfterStartTime())
+      .withMessage('endTime should be after startTime')
+      .run(req);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
       return;
     }
 
-    if (!DateUtil.isPastTime(currentTimeMs, startTimeMs)) {
-      res.send('startTime should be past time');
-      return;
-    }
+    const { filterBy: keyword, startTime, endTime } = req.query;
+    let startTimeISO;
+    let endTimeISO;
 
-    if (DateUtil.isTimeOlderByDays(currentTimeMs, startTimeMs, maxObservableDays)) {
-      res.send('startTime should be no more than 7 days ago');
-      return;
-    }
-    startTimeISO = new Date(startTime).toISOString();
-  }
+    if (startTime) startTimeISO = new Date(startTime).toISOString();
+    if (endTime) endTimeISO = new Date(endTime).toISOString();
 
-  if (endTime) {
-    endTimeMs = Date.parse(endTime);
-    if (Number.isNaN(endTimeMs)) {
-      res.send('endTime should be ISO 8601 format');
-      return;
-    }
-
-    if (DateUtil.isFutureTime(currentTimeMs, endTimeMs)) {
-      res.send('endTime should be present or past time');
-      return;
-    }
-
-    if (DateUtil.isTimeOlderByDays(currentTimeMs, endTimeMs, maxObservableDays)) {
-      res.send('endTime should be no more than 7 days ago');
-      return;
-    }
-
-    if (startTime && !DateUtil.isFutureTime(startTimeMs, endTimeMs)) {
-      res.send('endTime should be after startTime');
-    }
-    endTimeISO = new Date(endTime).toISOString();
-  }
-
-  const mentionData = await getMentions(keyword, startTimeISO, endTimeISO);
-  res.send(mentionData);
-});
+    const mentionData = await getMentions(keyword, startTimeISO, endTimeISO);
+    res.send(mentionData);
+  },
+);
 
 app.listen(port);
